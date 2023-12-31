0c0fc3cb442c5f5fd765c480ec163e2441531a56
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-12-30 23:56:28 -0600

    Demo home page

--- index_handler.go
+++ index_handler.go
@@ -27,6 +27,19 @@ func (h *IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	view := IndexModel{
 		Clips: clips,
+		Tags: []string{
+			"ポン", "挨拶", "センシティブ", "おやすみ",
+			"ヤンデレ", "ツンデレ", "圧", "かわいい", "英語",
+		},
+		VTubers: []string{
+			"博衣こより", "湊あくあ", "しぐれうい",
+			"白上フブキ", "夏色まつり", "百鬼あやめ",
+			"紫咲シオン", "癒月ちょこ", "大空スバル",
+			"月ノ美兎", "樋口楓", "猫又おかゆ",
+		},
+		Agencies: []string{
+			"ホロライブ", "にじさんじ", "ホロスターズ",
+		},
 	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
